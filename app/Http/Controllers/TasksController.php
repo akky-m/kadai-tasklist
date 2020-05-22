8fc4dbbcbf051ac40143f4d571ed24519e1b4e0e
akky-m <akiko30502376@gmail.com>
2020-05-22 13:58:16 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -40,8 +40,14 @@ class TasksController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    {
+ {
+           $this->validate($request, [
+            'status' => 'required|max:10', 
+            'content' => 'required'
+        ]);
+        
         $task = new Task;
+        $task->status = $request->status;   
         $task->content = $request->content;
         $task->save();
 
@@ -88,9 +94,15 @@ class TasksController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
-    {
+        {
+           $this->validate($request, [
+            'status' => 'required|max:10',   
+            'content' => 'required'
+        ]);
+        
         $task = Task::find($id);
         $task->content = $request->content;
+        $task->status = $request->status;   
         $task->save();
 
         return redirect('/');
